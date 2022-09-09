0ef284e4bcd5633ef88df7b1879312da573d5262
Dionessa <dionessaobi@gmail.com>
2022-09-09 13:46:09 -0700

    trailing whitespace error fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  * main - Entry point
  *
  * Decription: 'the program's decsription'
- * 
+ *
  * Return: will be 1 on success
  */
 int main(void)
@@ -11,4 +11,3 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-String result = str.trim();
