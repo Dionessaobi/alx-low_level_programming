55a18ed1704696e09c37a2ab3c61f1f616110e4c
Dionessa <dionessaobi@gmail.com>
2022-09-09 13:18:36 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <unistd.h>
 /**
- * main - Prints a text
+ * main - Entry point
+ *
+ * Decription: 'the program's decsription'
  * 
- * Return: Always 1 (Success)
+ * Return: will be 1 on success
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
