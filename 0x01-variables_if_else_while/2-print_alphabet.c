b6b4d09ffe1ab099a4354eb6623a9c4e763c8e86
Dionessa <dionessaobi@gmail.com>
2022-09-10 15:01:53 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - 'Entry point'
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	
+	putchar('\n');
+	return (0);
+}
