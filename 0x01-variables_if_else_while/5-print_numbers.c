d9ad2a6e1efb99ca62cd69e53635181e5755c423
Dionessa <dionessaobi@gmail.com>
2022-09-11 14:34:09 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+i#include <stdio.h>
 
 /**
  * main - Entry point
- * 
+ *
  * Description: 'the program's description'
- * 
+ *
  * Return: Always 0 (success)
  */
- int main(void)
- {
-    int a = 0;
+int main(void)
+{
+int a = 0;
 
-    while (a < 10)
-    {
-        printf("%d", a);
-        a++;
-    }
+while (a < 10)
+   
+{
+printf("%d", a);
+a++
+}
 
-    printf("\n");
-    return (0);
- }
- 
+printf("\n");
+return (0);
+}
