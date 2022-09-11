8391a766917fc7379a3008ac039adb122a28d3f2
Dionessa <dionessaobi@gmail.com>
2022-09-11 15:22:56 -0700

    fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,20 +8,20 @@
  * 
  * Return: Always 0 (success)
  */
- int main(void)
- {
-    int a;
+int main(void)
+{
+int a;
 
-    for (a = 48; a < 58; a++)
-    {
-        putchar(a)
-        if (a != 57)
-        {
-            putchar(',');
-            putchar('');
-        }
-    }
+for (a = 48; a < 58; a++)
+{
+putchar(a);
+if (a != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
 
-    putchar('\n');
-    return (0);
- }
+putchar('\n');
+return (0);
+}
