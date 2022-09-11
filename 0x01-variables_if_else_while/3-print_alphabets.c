ede9198bbf1b21a33285afc3abffe66d834f4d96
Dionessaobi <dionessaobi@gmail.com>
2022-09-11 21:32:26 +0100

    fixes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,16 +13,16 @@ char upper_alphabet = 'A';
 
 while (lower_alphabet <= 'z')
 {
-    putchar(lower_alphabet);
-    lower_alphabet++;
-    }
-    while (upper_alphabet <= 'Z')
-    {
-    putchar(upper_alphabet);
-    upper_alphabet++;
-    }
+putchar(lower_alphabet);
+lower_alphabet++;
+}
+while (upper_alphabet <= 'Z')
+{
+putchar(upper_alphabet);
+upper_alphabet++;
+}
 
-    putchar('\n');
-    return (0);
+putchar('\n');
+return (0);
 }
  
