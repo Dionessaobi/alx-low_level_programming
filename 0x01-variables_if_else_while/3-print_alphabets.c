301248195b527b457ddc3c9490d62c60a992139f
Dionessaobi <dionessaobi@gmail.com>
2022-09-11 21:18:34 +0100

    fixes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,27 +2,27 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Description: 'the program's description'
  * Return: Always 0 (success)
  */
- int main(void)
- {
-    char lower_alphabet = 'a';
-    char upper_alphabet = 'A';
+int main(void)
+{
+char lower_alphabet = 'a';
+char upper_alphabet = 'A';
 
-    while (lower_alphabet <= 'z')
-    {
-        putchar(lower_alphabet);
-        lower_alphabet++;
+while (lower_alphabet <= 'z')
+{
+    putchar(lower_alphabet);
+    lower_alphabet++;
     }
     while (upper_alphabet <= 'Z')
     {
-        putchar(upper_alphabet);
-        upper_alphabet++;
+    putchar(upper_alphabet);
+    upper_alphabet++;
     }
 
     putchar('\n');
     return (0);
- }
+}
  
