a54caed5ffe72946bd4ee2a8fd8847e7779b1499
Dionessa <dionessaobi@gmail.com>
2022-09-12 11:23:12 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,23 +13,21 @@ int main(void)
 
 	for (a = 0; a < 100; a++)
 	{
-		(b = 0); (b > 100); (b++);
+		if (a < b)
 		{
-			if (a < b)
+			putchar((a / 10) + 48);
+			putchar((b % 10) + 48);
+			putchar(' ');
+			putchar((b / 10) + 48);
+			putchar((b % 10) + 48);
+			if (a != 98 || b != 99)
 			{
-				putchar((a / 10) + 48);
-				putchar((b % 10) + 48);
+				putchar(',');
 				putchar(' ');
-				putchar((b / 10) + 48);
-				putchar((b % 10) + 48);
-				if (a != 98 || b != 99)
-				{
-					putchar(',');
-					putchar(' ');
-				}
 			}
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
