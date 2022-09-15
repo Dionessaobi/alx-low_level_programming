b2f3208dfdbe5ae3228b58c78361442bbb91811c
Dionessaobi <dionessaobi@gmail.com>
2022-09-15 18:47:01 +0100

    idk

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,17 +7,17 @@
  * 
  * Return: Always 0 (success)
  */
- int main(void)
- {
-    int a = 0;
+int main(void)
+{
+int a = 0;
 
-    while (a < 10)
-    {
-        printf("%d", a);
-        a++;
-    }
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
 
-    printf("\n");
-    return (0);
- }
+printf("\n");
+return (0);
+}
  
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,17 +7,17 @@
  * 
  * Return: Always 0 (success)
  */
- int main(void)
- {
-    int a = 48;
+int main(void)
+{
+int a = 48;
 
-    while (a < 58)
-    {
-        putchar(a);
-        a++;
-    }
+while (a < 58)
+{
+putchar(a);
+a++;
+}
 
-    putchar('\n');
-    return (0);
- }
+putchar('\n');
+return (0);
+}
  
