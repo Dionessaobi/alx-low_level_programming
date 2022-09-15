380ea19e35898ed4eec02820374240be1a85f7d1
Dionessa <dionessaobi@gmail.com>
2022-09-15 10:12:33 -0700

    tireddd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_10x - function to print abc 10 times
+ * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
  */
-void print_alphabet_10x(void)
+void print_alphabet_x10(void)
 {
 	char c, i;
 
