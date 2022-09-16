e94cc667582f625bac39428155847bdc63dd56c8
Dionessa <dionessaobi@gmail.com>
2022-09-16 08:22:00 -0700

    :sob:

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,10 +19,10 @@ void jack_bauer(void)
 	mins_remainder = minutes % 10;
 	hours_remainder = hours % 10;
 	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0')
+	_putchar(hours_remainder + '0');
 	_putchar(':');
 	_putchar(minutes / 10 + '0');
-	_putchar(minutes_remainder + '0');
+	_putchar(mins_remainder + '0');
 	minutes++;
 	_putchar('\n');
 	}
