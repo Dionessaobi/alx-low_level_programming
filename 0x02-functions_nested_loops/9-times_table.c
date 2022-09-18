5ea016ea41e1aecff0eb213b72d3d306e23403ef
Dionessa <dionessaobi@gmail.com>
2022-09-18 05:38:47 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mult = 0; mult <= 0; mult++)
+		for (mult = 1; mult <= 0; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-		
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
