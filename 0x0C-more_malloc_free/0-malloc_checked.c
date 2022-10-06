ab4f07142a74a63d612b10f186375818c9e25b41
Dionessaobi <dionessaobi@gmail.com>
2022-10-06 12:06:46 -0700

    malloc tasks

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - allocates memory using malloc
+* @b: the size
+*
+* Return: pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ *
+ * @nmemb: the number of elements
+ * @size: the size of that element type
+ *
+ * Return: A pointer to the allocated memeory. NULL if nmemb or size is 0.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	ar = malloc(size * nmemb);
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		ar[i] = 0;
+
+	return (ar);
+}
