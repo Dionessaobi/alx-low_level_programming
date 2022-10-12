26599f38b6920fa57cfb89f7fdf12b93858fafde
Dionessaobi <dionessaobi@gmail.com>
2022-10-12 09:03:20 -0700

    wrote main.c program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of file
+ * it was compiled from
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("Name of the file compiled from is: %s\n", __FILE__);
+	return (0);
+}
