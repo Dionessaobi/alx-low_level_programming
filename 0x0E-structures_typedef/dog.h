337300bf88dd21dea37900618b0a6a13afd391fc
Dionessaobi <dionessaobi@gmail.com>
2022-10-13 09:36:31 -0700

    :sob:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - the name of the struct
+ * main - define a new type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: description of the dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
